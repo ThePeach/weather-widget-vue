885fdd78dcdb444255f35cdadfddc198211302b1
Matteo 'Peach' Pescarin <peach@smartart.it>
2019-02-06 14:08:34 +0000

    removed onload event and made script async and defer

--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import weatherWidget from "./components/weather-widget/";
 // Don't warn about using the dev version of Vue in development.
 Vue.config.productionTip = process.env.NODE_ENV === "production";
 
-window.addEventListener("load", function() {
-  const widgetDemo = new Vue({
-    el: "#demo",
-    data: {
-      title: "weather-widget demo"
-    },
-    components: {
-      "weather-widget": weatherWidget
-    }
-  });
+const widgetDemo = new Vue({
+  el: "#demo",
+  data: {
+    title: "weather-widget demo"
+  },
+  components: {
+    "weather-widget": weatherWidget
+  }
 });
